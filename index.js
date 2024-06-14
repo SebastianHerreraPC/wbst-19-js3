7ff2da9bb45df5cb959b777e5c87156f360b61f6
SebastianHerreraPc <sebehepe00@gmail.com>
2024-06-13 23:03:43 -0600

    array

---
+++ index.js
@@ -0,0 +1,20 @@
+//primer array
+
+let months = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
+months[1];
+
+console.log(months[1]);
